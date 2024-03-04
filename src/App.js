c5d6ebda7e8b7ded724f1e45ea9131a6b73ddb8f
Nandini-Jangiti <jangiti.nandini@ust.com>
2024-03-04 11:14:00 +0530

    Assessment-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import MainComponent from './components/MainComponent';
 
-function App() {
+
+const App = () => {
+  const productsData = [
+    {
+      id: 1,
+      url: '/images/product-1.png',
+      name: 'Brown eggs',
+      description: 'Raw organic brown eggs',
+      category: 'dairy',
+      price: 'Rs. 100',
+      rating: 4.5
+    },
+    {
+      id: 2,
+      url: '/images/product-2.png',
+      name: 'Sweet fresh stawberry',
+      description: 'Sweet fresh stawberry',
+      category: 'fruit',
+      price: 'Rs. 150',
+      rating: 4.8
+    },
+    {
+      id: 4,
+      url: "/images/orange.png",
+      name: "Fresh orange",
+      description: "Fresh orange juice",
+      category: "fruit",
+      price: "Rs.200",
+      rating: 4.6
+    },
+
+    {
+      id: 5,
+      url: "/images/yogurt.png",
+      name: "Strawberry yogurt",
+      description: "Strawberry yogurt with berries",
+      category: "dairy",
+      price: "Rs. 300",
+      rating: 4.4
+    },
+
+    {
+      id: 6,
+      url: "/images/product-veg1.png",
+      name: "Organic tomato",
+      description: "Fresh Organic tomato ",
+      category: "vegetables",
+      price: "Rs. 150",
+      rating: 4.6
+    },
+    {
+      id: 7,
+      url: "/images/product-veg.png",
+      name: "Basil",
+      description: "Fresh basil leaves",
+      category: "vegetables",
+      price: "Rs. 100",
+      rating: 4.5
+    }
+
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="App">
+        <MainComponent products={productsData} />
+      </div>
+      {/* <Footer/> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-h">
+                    <h3>Home</h3>
+                </div>
+               
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
